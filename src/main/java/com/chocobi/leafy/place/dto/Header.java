e62370eb35bf61b6eb0f8b1aa8053d583187e37e
eeun1225 <eeun9108@gmail.com>
2025-06-24 13:35:29 +0900

    feat: @XmlAccessorType 어노테이션 추가

--- src/main/java/com/chocobi/leafy/place/dto/Header.java
+++ src/main/java/com/chocobi/leafy/place/dto/Header.java
@@ -1,8 +1,11 @@
 package com.chocobi.leafy.place.dto;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
 import lombok.Data;
 
 @Data
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Header {
     private String resultCode;
     private String resultMsg;
