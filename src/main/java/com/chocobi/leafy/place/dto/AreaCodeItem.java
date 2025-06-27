2f4b1e070d84dd1252042db99ca2ad473c17fff0
eeun1225 <eeun9108@gmail.com>
2025-06-27 12:20:36 +0900

    refactor: Area 관련 DTO 패키지 분리 및 불필요한 클래스 삭제 (#2)

--- src/main/java/com/chocobi/leafy/place/dto/AreaCodeItem.java
+++
@@ -1,9 +0,0 @@
-package com.chocobi.leafy.place.dto;
-
-import lombok.Data;
-
-@Data
-public class AreaCodeItem {
-    private String code;
-    private String name;
-}
--- src/main/java/com/chocobi/leafy/place/dto/AreaCodeResponse.java
+++
@@ -1,8 +0,0 @@
-package com.chocobi.leafy.place.dto;
-
-import lombok.Data;
-
-@Data
-public class AreaCodeResponse<T> {
-    private Response<T> response;
-}
