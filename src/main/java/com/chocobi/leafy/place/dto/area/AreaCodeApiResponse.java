01ed1cd14454e4ac4918399215ba5373256145f2
eeun1225 <eeun9108@gmail.com>
2025-08-22 12:58:31 +0900

    refactor: Areacode 관련 Class 삭제 (#18)

--- src/main/java/com/chocobi/leafy/place/dto/area/AreaCodeApiResponse.java
+++
@@ -1,9 +0,0 @@
-package com.chocobi.leafy.place.dto.area;
-
-import com.chocobi.leafy.place.dto.common.Response;
-import lombok.Data;
-
-@Data
-public class AreaCodeApiResponse<T> {
-    private Response<T> response;
-}
