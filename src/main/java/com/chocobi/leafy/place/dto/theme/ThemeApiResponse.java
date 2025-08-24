7a62fea9a1248d59fac33b5031ce72ca6bec864d
eeun1225 <eeun9108@gmail.com>
2025-08-24 22:21:38 +0900

    refactor: 장소별 API DTO 패키지 분리 (#18)

--- src/main/java/com/chocobi/leafy/place/dto/theme/ThemeApiResponse.java
+++
@@ -1,9 +0,0 @@
-package com.chocobi.leafy.place.dto.theme;
-
-import com.chocobi.leafy.place.dto.common.Response;
-import lombok.Data;
-
-@Data
-public class ThemeApiResponse<T> {
-    private Response<T> response;
-}
