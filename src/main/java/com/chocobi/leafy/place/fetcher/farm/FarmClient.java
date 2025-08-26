eac6d28c25acd45a32fa65c7bf186ee5d68bf9ba
eeun1225 <eeun9108@gmail.com>
2025-08-26 18:03:02 +0900

    refactor: Client Class 메서드 수정 (#18)

--- src/main/java/com/chocobi/leafy/place/fetcher/farm/FarmClient.java
+++ src/main/java/com/chocobi/leafy/place/fetcher/farm/FarmClient.java
@@ -4,21 +4,29 @@ import com.chocobi.leafy.place.common.util.PlaceConstants;
 import com.chocobi.leafy.place.fetcher.farm.dto.FarmDetailApiResponse;
 import com.chocobi.leafy.place.fetcher.farm.dto.FarmListApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriBuilder;
 
-import java.net.URI;
 
 @Component
 @RequiredArgsConstructor
 public class FarmClient {
     private final WebClient farmWebClient;
 
+    @Value("${farm.api.key}")
+    private String apiKey;
+
     // 목록 조회
     public FarmListApiResponse fetchFarmList() {
         return farmWebClient.get()
-                .uri(this::buildFarmListUri)
+                .uri(uriBuilder -> uriBuilder
+                        .path(PlaceConstants.FARM_LIST)
+                        .queryParam("apiKey", apiKey)
+                        .queryParam("numOfRows", PlaceConstants.DEFAULT_NUM_OF_ROWS)
+                        .queryParam("pageNo", PlaceConstants.PAGE_NO)
+                        .build()
+                )
                 .retrieve()
                 .bodyToMono(FarmListApiResponse.class)
                 .block();
@@ -27,23 +35,14 @@ public class FarmClient {
     // 상세 조회
     public FarmDetailApiResponse fetchFarmDetail(String contentId) {
         return farmWebClient.get()
-                .uri(uriBuilder -> buildFarmDetailUri(uriBuilder, contentId))
+                .uri(uriBuilder -> uriBuilder
+                        .path(PlaceConstants.FARM_DETAIL)
+                        .queryParam("apiKey", apiKey)
+                        .queryParam("cntntsNo", contentId)
+                        .build()
+                )
                 .retrieve()
                 .bodyToMono(FarmDetailApiResponse.class)
                 .block();
-    }
-
-
-    private URI buildFarmListUri(UriBuilder builder) {
-        return builder.path(PlaceConstants.FARM_LIST)
-                .queryParam("numOfRows", PlaceConstants.DEFAULT_NUM_OF_ROWS)
-                .queryParam("pageNo", PlaceConstants.PAGE_NO)
-                .build();
-    }
-
-    private URI buildFarmDetailUri(UriBuilder builder, String contentId) {
-        return builder.path(PlaceConstants.FARM_DETAIL)
-                .queryParam("cntntsNo", contentId)
-                .build();
     }
 }
--- src/main/java/com/chocobi/leafy/place/fetcher/theme/ThemeClient.java
+++ src/main/java/com/chocobi/leafy/place/fetcher/theme/ThemeClient.java
@@ -4,32 +4,34 @@ import com.chocobi.leafy.place.common.util.PlaceConstants;
 import com.chocobi.leafy.place.fetcher.theme.dto.ThemeApiResponse;
 import com.chocobi.leafy.place.fetcher.theme.dto.ThemeItem;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriBuilder;
 
-import java.net.URI;
+import java.util.Collections;
 
 @Component
 @RequiredArgsConstructor
 public class ThemeClient {
-    private final WebClient themeWebClient;
+    private final WebClient cultureWebClient;
+
+    @Value("${theme.api.key}")
+    private String serviceKey;
 
     public ThemeApiResponse<ThemeItem> fetchThemePlaces() {
-        return themeWebClient.get()
-                .uri(this::buildThemeUri)
+        return cultureWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(PlaceConstants.THEME_PATH)
+                        .queryParam("serviceKey", serviceKey)
+                        .queryParam("numOfRows", PlaceConstants.MAX_NUM_OF_ROWS)
+                        .queryParam("pageNo", PlaceConstants.PAGE_NO)
+                        .queryParam("_type", PlaceConstants.RESPONSE_TYPE_JSON)
+                        .build()
+                )
                 .header("Accept", "application/json")
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<ThemeApiResponse<ThemeItem>>() {})
                 .block();
-    }
-
-    private URI buildThemeUri(UriBuilder builder) {
-        return builder.path(PlaceConstants.THEME_PATH)
-                .queryParam("numOfRows", PlaceConstants.MAX_NUM_OF_ROWS)
-                .queryParam("pageNo", PlaceConstants.PAGE_NO)
-                .queryParam("_type", PlaceConstants.RESPONSE_TYPE_JSON)
-                .build();
     }
 }
