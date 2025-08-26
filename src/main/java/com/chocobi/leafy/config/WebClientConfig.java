2e36a27370a4d47c1cb8a451ce0f2e2f828fe675
eeun1225 <eeun9108@gmail.com>
2025-08-26 18:01:46 +0900

    feat: WebClientConfig 수정 및 NaverWebClient 추가 (#20)

--- src/main/java/com/chocobi/leafy/config/WebClientConfig.java
+++ src/main/java/com/chocobi/leafy/config/WebClientConfig.java
@@ -7,62 +7,66 @@ import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
-import java.util.Map;
 
 @Configuration
 public class WebClientConfig {
 
-    // 생태관광 API, 관광사진 API
+    // 생태관광 API
     @Bean
-    public WebClient tourWebClient(@Value("${tour.api.base.url}") String tourApiBaseUrl,
-                                   @Value("${tour.api.key}") String tourApiKey) {
-        return createWebClient(tourApiBaseUrl, tourApiKey);
+    public WebClient tourWebClient(@Value("${tour.api.base.url}") String tourApiBaseUrl) {
+        return createWebClient(tourApiBaseUrl);
+    }
+
+    // 테마관광 API, 농어촌체험 API
+    @Bean
+    public WebClient cultureWebClient(@Value("${culture.api.base.url}") String cultureBaseUrl) {
+        return createWebClient(cultureBaseUrl);
     }
 
     @Bean
-    public WebClient ruralWebClient(@Value("${culture.api.base.url}") String cultureBaseUrl,
-                                    @Value("${rural.api.key}") String ruralApiKey) {
-        return createWebClient(cultureBaseUrl, ruralApiKey);
+    public WebClient farmWebClient(@Value("${farm.api.base.url}") String farmBaseUrl) {
+        return createWebClient(farmBaseUrl);
     }
 
     @Bean
-    public WebClient themeWebClient(@Value("${culture.api.base.url}") String cultureBaseUrl,
-                                    @Value("${theme.api.key}") String themeApiKey) {
-        return createWebClient(cultureBaseUrl, themeApiKey);
+    public WebClient kakaoWebClient(@Value("${kakao.api.base.url}") String kakaoBaseUrl) {
+        return createWebClient(kakaoBaseUrl);
     }
 
     @Bean
-    public WebClient farmWebClient(@Value("${farm.api.base.url}") String farmBaseUrl,
-                                   @Value("${farm.api.key}") String farmApiKey) {
-        return createWebClient(farmBaseUrl, farmApiKey);
-    }
+    public WebClient naverWebClient(@Value("${naver.base.url}") String naverBaseUrl,
+                                    @Value("${naver.client.id}") String clientId,
+                                    @Value("${naver.client.secret}") String clientSecret) {
 
-    @Bean
-    public WebClient kakaoWebClient(@Value("${kakao.api.base.url}") String kakaoBaseUrl,
-                                    @Value("${kakao.api.key}") String kakaoApiKey) {
         return WebClient.builder()
-                .baseUrl(kakaoBaseUrl)
-                .defaultHeader("Authorization", "KakaoAK " + kakaoApiKey)
+                .baseUrl(naverBaseUrl)
+                .defaultHeader("X-Naver-Client-Id", clientId)
+                .defaultHeader("X-Naver-Client-Secret", clientSecret)
+                .uriBuilderFactory(createUriBuilderFactory(naverBaseUrl))
                 .build();
     }
 
-    private WebClient createWebClient(String baseUrl, String serviceKey) {
-        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(baseUrl);
-        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
 
+    private WebClient createWebClient(String baseUrl) {
         return WebClient.builder()
-                .uriBuilderFactory(factory)
+                .uriBuilderFactory(createUriBuilderFactory(baseUrl))
                 .exchangeStrategies(commonExchangeStrategies())
                 .baseUrl(baseUrl)
-                .defaultUriVariables(Map.of("ServiceKey", serviceKey))
                 .build();
+    }
+
+    private DefaultUriBuilderFactory createUriBuilderFactory(String baseUrl) {
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(baseUrl);
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+        return factory;
     }
 
     private ExchangeStrategies commonExchangeStrategies() {
         return ExchangeStrategies.builder()
-                .codecs(configurer -> configurer
-                        .defaultCodecs()
-                        .maxInMemorySize(2 * 1024 * 1024))
+                .codecs(configurer -> {
+                    configurer.defaultCodecs()
+                            .maxInMemorySize(2 * 1024 * 1024);
+                    })
                 .build();
     }
 }
