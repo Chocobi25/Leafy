eb6aeffd0afdf78b5db892ac81fb130c815abbd8
eeun1225 <eeun9108@gmail.com>
2025-08-26 18:18:33 +0900

    feat: BatchConfig에 Image 추가 (#19)

--- src/main/java/com/chocobi/leafy/config/BatchConfig.java
+++ src/main/java/com/chocobi/leafy/config/BatchConfig.java
@@ -1,15 +1,17 @@
 package com.chocobi.leafy.config;
 
-import com.chocobi.leafy.place.batch.PlaceGeocodeProcessor;
-import com.chocobi.leafy.place.batch.PlaceStagingTasklet;
-import com.chocobi.leafy.place.batch.PlaceWriter;
+import com.chocobi.leafy.place.batch.*;
+import com.chocobi.leafy.place.entity.Image;
 import com.chocobi.leafy.place.entity.Place;
 import com.chocobi.leafy.place.entity.PlaceStaging;
+import com.chocobi.leafy.place.fetcher.image.ImageSearchClient;
+import com.chocobi.leafy.place.repository.ImageRepository;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -18,6 +20,8 @@ import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilde
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
 
 
 @Configuration
@@ -29,8 +33,14 @@ public class BatchConfig {
     private final EntityManagerFactory entityManagerFactory;
 
     private final PlaceStagingTasklet placeStagingTasklet;
+
     private final PlaceGeocodeProcessor placeGeocodeProcessor;
     private final PlaceWriter placeWriter;
+
+    private final ImageSearchClient imageSearchClient;
+    private final ImageRepository imageRepository;
+    private final PlaceImageProcessor placeImageProcessor;
+    private final ImageWriter imageWriter;
 
     @Bean
     public Step placeStagingStep() {
@@ -50,6 +60,16 @@ public class BatchConfig {
     }
 
     @Bean
+    public ItemReader<Place> placeReader() {
+        return new JpaPagingItemReaderBuilder<Place>()
+                .name("placeReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(100)
+                .queryString("SELECT p FROM Place p")
+                .build();
+    }
+
+    @Bean
     public Step placeProcessStep() {
         return new StepBuilder("placeProcessStep", jobRepository)
                 .<PlaceStaging, Place>chunk(100, transactionManager)
@@ -60,10 +80,28 @@ public class BatchConfig {
     }
 
     @Bean
+    @StepScope
+    public PlaceImageProcessor placeImageProcessor() {
+        return new PlaceImageProcessor(imageSearchClient, imageRepository);
+    }
+
+    @Bean
+    public Step imageStep() {
+        return new StepBuilder("imageStep", jobRepository)
+                .<Place, List<Image>>chunk(100, transactionManager)
+                .reader(placeReader())
+                .processor(placeImageProcessor)
+                .writer(imageWriter)
+                .build();
+    }
+
+
+    @Bean
     public Job placeDataJob() {
         return new JobBuilder("placeDataJob", jobRepository)
-                .start(placeStagingStep()) // 1단계 Step
-                .next(placeProcessStep())   // 2단계 Step
+                .start(placeStagingStep())
+                .next(placeProcessStep())
+                .next(imageStep())
                 .build();
     }
 }
