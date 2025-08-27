b0db3d1509af1fb974f88e4c93e6688dda71ca14
eeun1225 <eeun9108@gmail.com>
2025-08-27 16:56:14 +0900

    refactor: Application DB 설정 삭제 (#18)

--- src/main/java/com/chocobi/leafy/LeafyApplication.java
+++ src/main/java/com/chocobi/leafy/LeafyApplication.java
@@ -1,7 +1,5 @@
 package com.chocobi.leafy;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeafyApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		String dbPassword = dotenv.get("DB_PASSWORD");
-		System.setProperty("DB_PASSWORD",
-				dbPassword != null ? dbPassword : "기본비밀번호나 예외처리");
-
 		SpringApplication.run(LeafyApplication.class, args);
 	}
 }
