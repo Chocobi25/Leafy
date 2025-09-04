506961d611773941eb1d190724f7911cd90e4e71
eeun1225 <eeun9108@gmail.com>
2025-09-04 13:43:08 +0900

    feat: FCM sdk 초기 설정 구현 (#26)
    - build.gradle dependency 추가
    - FirebaseConfig 작성

---
+++ src/main/java/com/chocobi/leafy/config/FirebaseConfig.java
@@ -0,0 +1,35 @@
+package com.chocobi.leafy.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Configuration
+@Slf4j
+public class FirebaseConfig {
+    @Value("${firebase.config.path}")
+    private String firebaseConfigPath;
+
+
+    @PostConstruct
+    public void init() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream());
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(googleCredentials)
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+            log.info("Firebase application has been initialized");
+        }
+    }
+}
