3af49090996f173bbf54283875572029b8f2310f
eeun1225 <eeun9108@gmail.com>
2025-09-04 15:47:51 +0900

    feat: FCMController Test용 메서드 추가 (#26)

--- src/main/java/com/chocobi/leafy/fcm/controller/FCMController.java
+++ src/main/java/com/chocobi/leafy/fcm/controller/FCMController.java
@@ -3,6 +3,7 @@ package com.chocobi.leafy.fcm.controller;
 import com.chocobi.leafy.fcm.service.FCMService;
 import com.chocobi.leafy.fcm.dto.FCMTokenDTO;
 import com.chocobi.leafy.fcm.service.UserDeviceService;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -38,6 +39,15 @@ public class FCMController {
 
         log.info("사용자 ID {}로부터 FCM 토큰 삭제 요청 수신.", userId);
         userDeviceService.unregisterToken(userId, fcmTokenDTO.getFcmToken());
+
+        return ResponseEntity.ok().build();
+    }
+
+    // TEST
+    @PostMapping("/test-send")
+    public ResponseEntity<Void> testSend(@AuthenticationPrincipal Long userId,
+                                         @RequestBody FCMTokenDTO fcmTokenDTO) throws FirebaseMessagingException {
+        fcmService.sendNotificationToSingleDevice(fcmTokenDTO.getFcmToken(), "테스트 알림", "테스트 메시지입니다.");
 
         return ResponseEntity.ok().build();
     }
