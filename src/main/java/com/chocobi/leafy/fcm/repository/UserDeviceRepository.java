0b8ce18935928d0446d656345ebcc47b26d08cc1
eeun1225 <eeun9108@gmail.com>
2025-09-04 13:56:20 +0900

    feat: UserDevice Entity 및 Repository 생성 (#26)
    - 한 명의 사용자(User)에게 여러 개의 디바이스 토큰을 연결하기 위해 추가

---
+++ src/main/java/com/chocobi/leafy/fcm/repository/UserDeviceRepository.java
@@ -0,0 +1,8 @@
+package com.chocobi.leafy.fcm.repository;
+
+import com.chocobi.leafy.fcm.entity.UserDevice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDeviceRepository extends JpaRepository<UserDevice, Long> {
+
+}
