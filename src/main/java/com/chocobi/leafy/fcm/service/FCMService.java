a74462faa489cc9711f4282fdb8bcb7f2fbde29a
eeun1225 <eeun9108@gmail.com>
2025-09-09 00:06:34 +0900

    feat: FCM 알림 전송 페이로드 및 메서드 추가 (#33)

--- src/main/java/com/chocobi/leafy/fcm/service/FCMService.java
+++ src/main/java/com/chocobi/leafy/fcm/service/FCMService.java
@@ -5,11 +5,26 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class FCMService {
-    public void sendNotification(String fcmToken, String title, String body) throws FirebaseMessagingException {
+
+    // 데이터 페이로드를 포함한 알림 전송
+    public void sendDataNotification(String fcmToken, String title, String body, Map<String, String> data) throws FirebaseMessagingException {
+        Message message = Message.builder()
+                .setToken(fcmToken)
+                .setNotification(createNotification(title, body))
+                .putAllData(data) // 데이터 페이로드를 메시지에 추가
+                .build();
+
+        FirebaseMessaging.getInstance().send(message);
+    }
+
+    // 페이로드 X
+    public void sendSimpleNotification(String fcmToken, String title, String body) throws FirebaseMessagingException {
         Message message = Message.builder()
                 .setToken(fcmToken)
                 .setNotification(createNotification(title, body))
@@ -17,6 +32,7 @@ public class FCMService {
 
         FirebaseMessaging.getInstance().send(message);
     }
+
     private Notification createNotification(String title, String body) {
         return Notification.builder()
                 .setTitle(title)
